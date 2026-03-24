f486a44bd2f3dbe9108d5926937d8b17b1f83a08
Tiago-Sbeghen <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-03-24 15:50:47 -0300

    up - transformei os blocos em botões pressionaveis e estilizei eles utilizando a tag "button", utlizei o Js para quando algum botão for pressionado aparecer na tela o respectivo algarismo.

--- logica.js
+++ logica.js
@@ -1,4 +1,25 @@
 
-function soma(){
+let display = document.getElementById("display");
+let valorAtual = "";
 
-};
+let botoes = document.querySelectorAll("button");
+
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+        let valor = botao.innerText;
+        tratarClique(valor);
+    });
+});
+
+
+function tratarClique(valor){
+
+    if (valor === "="){
+        calcular();
+    } else {
+        valorAtual += valor;
+        display.innerText = valorAtual;
+    }
+
+}
