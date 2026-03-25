45218cf4ccb3e47accadae71eda2651dee23f9e0
Tiago-Sbeghen <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-03-25 11:58:55 -0300

    add - adicionei a funções "AC" e "C" para que quando os respectivos botões forem pressinados apagar um algarismo ou limpar tudo.

--- logica.js
+++ logica.js
@@ -13,13 +13,28 @@ botoes.forEach(botao => {
 });
 
 
-function tratarClique(valor){
+function tratarClique(valor) {
 
-    if (valor === "="){
+    if (valor === "=") {
         calcular();
+    } else if (valor === "AC") {
+        limpartudo();
+    } else if (valor === "C") {
+        apagarUltimo();
     } else {
         valorAtual += valor;
         display.innerText = valorAtual;
     };
+
+};
+
+function limpartudo() {
+    valorAtual = "";
+    display.innerText = "";
+};
+
+function apagarUltimo() {
+    valorAtual = valorAtual.slice(0, -1);
+    display.innerText = valorAtual;
 
 };
